5445bda2a4ae983cadef9fd161d5afa14a159f25
Leeroy <leeroy.sixvalues.com>
2025-02-09 20:00:46 +0200

    Refactor Open Graph metadata order and update image URL

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="uk">
+      <meta property="og:type" content="article" />
       <meta
         property="og:title"
         content="Drako Schule | Сучасний простір для вивчення мов"
@@ -59,18 +60,16 @@ export default function RootLayout({
         property="og:description"
         content="Drako Schule — це місце, де навчання перетворюється на казку! Ми пропонуємо якісні заняття, цікаву подачу матеріалу та індивідуальний підхід до кожного. Приєднуйтесь до нас та зануртесь у Всесвіт драконів!"
       />
-      <meta property="og:url" content="https://ds-landing-weld.vercel.app/" />
       <meta
         property="og:image"
-        content="https://res.cloudinary.com/di1lzfsaa/image/upload/v1738929811/drako-schule-og:image.jpg"
+        content="https://res.cloudinary.com/di1lzfsaa/image/upload/v1738929811/drako-schule-banner.jpg"
       />
       <meta property="og:image:alt" content="Drako Schule banner" />
+      <meta property="og:url" content="https://ds-landing-weld.vercel.app/" />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta property="og:image:type" content="image/jpeg" />
-      <meta property="og:type" content="article" />
       <meta property="og:site_name" content="Drako Schule" />
-      <meta property="og:image:alt" content="Опис зображення" />
 
       <body
         className={`${montserrat.variable} ${delaGothicOne.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
